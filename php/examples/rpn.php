e628b206f098a8edf35cdd01411c70beaf0f203b
Pawel Wilk <pwilkmielno@gmail.com>
2013-10-27 16:54:13 +0100

    validateInfix and comments

--- php/examples/rpn.php
+++ php/examples/rpn.php
@@ -3,6 +3,6 @@ require('../lib/ReversedPolishNotation.class.php');
 
 $infixExpression = "3+4*2/(1-5)^2";
 
-$rpn = new ReversedPolishNotation($infixExpression, array(), true);
+$rpn = new ReversedPolishNotation($infixExpression, true);
 
 echo "\n".$rpn->getExpression()."\n";
--- php/lib/ReversedPolishNotation.class.php
+++ php/lib/ReversedPolishNotation.class.php
@@ -4,15 +4,20 @@ class ReversedPolishNotation {
   protected $variables = array();
   protected $operators = array('+', '-', '*', '/', '%', '^');
 
-  public function __construct($expression = null, $variables = array(), $infix = false) {
-    if ($infix) {
-      $this->fromInfixNotation($expression);
-    } else {
-      $this->expression = $expression;
+  public function __construct($expression = null, $infix = false, $variables = array()) {
+    if (!is_null($expression)) {
+      $this->setExpression($expression, $infix);
     }
     $this->variables = $variables;
   }
 
+  /**
+   * Convert infix expression to RPN(postfix)
+   *
+   * @param string $expression
+   *
+   * @throws InvalidArgumentException
+   */
   public function fromInfixNotation($expression) {
     $stack = array();
     $output = '';
@@ -20,6 +25,9 @@ class ReversedPolishNotation {
 
     $expression = preg_replace('/\s+/', '', $expression); //removing all whitespaces
     $expression_length = strlen($expression);
+    if (!$this->validateInfix($expression)) {
+      throw new InvalidArgumentException('Infix expression is not correct!');
+    }
 
     while ($i < $expression_length) {
       if (preg_match('#[a-z0-9]#Uis', $expression[$i])) {
@@ -68,7 +76,13 @@ class ReversedPolishNotation {
     $this->expression = trim($output);
   }
 
-  public function validate() {
+  /**
+   * Validate RPN(postfix) expression
+   *
+   * @param string $expression
+   * @return boolean
+   */
+  public function validateRPN($expression) {
     //
   }
 
@@ -92,18 +106,47 @@ class ReversedPolishNotation {
     //
   }
 
+  /**
+   * Return RPN(postfix) expression
+   *
+   * @return string
+   */
   public function getExpression() {
     return $this->expression;
   }
 
-  public function setExpression($expression = null) {
-    //
+  /**
+   * Sets RPN(postfix) expression
+   *
+   * @param string $expression
+   * @param boolean $infix defines notation of expression
+   *
+   * @throws InvalidArgumentException
+   */
+  public function setExpression($expression, $infix = false) {
+    if ($infix) {
+      $this->fromInfixNotation($expression);
+    } else {
+      $this->expression = $expression;
+    }
   }
 
+  /**
+   * Checks if char is supported operator operator is supported
+   *
+   * @param char $char
+   * @return boolean
+   */
   protected function isOperator($char) {
     return in_array($char, $this->operators);
   }
 
+  /**
+   * Method return weight for arithmetic operator
+   *
+   * @param char $operator
+   * @return integer
+   */
   protected function operatorsPriority($operator) {
     if ($operator == '^') {
       return 3;
@@ -115,6 +158,31 @@ class ReversedPolishNotation {
       return 1;
     }
     return 0;
+  }
+
+  /**
+   * Very simple method which quickly validate infix expression, not perfect but roks for most of typical errors
+   *
+   * @param string $expression
+   * @return boolean
+   */
+  protected function validateInfix($expression) {
+    $parenthesis_level = 0;
+    for ($i = 0; $i < strlen($expression); $i++) {
+      if (!$this->isOperator($expression[$i]) && !preg_match('#[0-9a-z()\s]#Uis', $expression[$i])) {
+        return false;
+      }
+      if ($expression[$i] == '(') {
+        $parenthesis_level++;
+      }
+      if ($expression[$i] == ')') {
+        $parenthesis_level--;
+      }
+      if ($parenthesis_level < 0) {
+        return false;
+      }
+    }
+    return $parenthesis_level == 0;
   }
 
   public function __toString() {
